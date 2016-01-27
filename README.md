d134d498968a7a1acda9755f447a170f6ce60ddd
Spencer Kelly <spencermountain@gmail.com>
2016-01-27 18:52:00 -0500

    get rolling with fr wordnet

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 
-:boom: nlp-compromise en francais :boom:
+:boom: C'est [nlp-compromise](https://github.com/nlp-compromise/nlp_compromise) en francais! :boom:
 
 Traitement automatique du langage naturel en client font
+
+il est fabrique avec la wordnet fracais, (WOLF)[http://alpage.inria.fr/~sagot/wolf-en.html]:
+* 10k adjectifs
+* 12k verbes
+* 2k adverbes
+* 70k noms
---
+++ learn/wolf/parse_wolf.js
@@ -0,0 +1,30 @@
+'use strict';
+var parser = require('xml2json');
+let fs = require('fs');
+
+let xml = fs.readFileSync(__dirname + '/wolf-1.0b4.xml', 'utf8');
+// let xml = fs.readFileSync(__dirname + '/tiny.xml', 'utf8');
+// xml to json
+// var xml = '<foo>bar</foo>';
+var json = JSON.parse(parser.toJson(xml));
+
+let words = [];
+
+let len = json.WN.SYNSET.length;
+for(var i = 0; i < len; i++) {
+  if (json.WN.SYNSET[i].SYNONYM.LITERAL !== '_EMPTY_') {
+    if (json.WN.SYNSET[i].POS !== 'n') {
+      continue;
+    }
+    let str = json.WN.SYNSET[i].SYNONYM.LITERAL['$t'];
+    if (str) {
+      words.push(str);
+    } else {
+      json.WN.SYNSET[i].SYNONYM.LITERAL.forEach(function(o) {
+        words.push(o['$t']);
+      });
+    }
+  }
+}
+
+console.log(JSON.stringify(words, null, 2));
