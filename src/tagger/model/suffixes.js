23eb6ff6bdf6274274be35fbdc2f919616867241
spencer kelly <spencerkelly86@gmail.com>
2022-02-28 14:26:43 -0500

    some suffixes

--- src/tagger/model/suffixes.js
+++ src/tagger/model/suffixes.js
@@ -1,12 +1,25 @@
 const rb = 'Adverb'
+const nn = 'Noun'
+const vb = 'Verb'
+
+
 export default [
   null,
   null,
   {
     //2-letter
+    'ce': nn,
+    'ur': nn,
+    'ge': nn,
+    'ie': nn,
+    'er': vb,
+    'ée': vb,
+    'és': vb,
   },
   {
     //3-letter
+    'ité': nn,
+    'ées': vb
   },
   {
     //4-letter
