c319894c6540091e6967ad2766602dbc9194e68e
spencer kelly <spencerkelly86@gmail.com>
2022-09-02 16:40:26 -0400

    80 percent, more tests

--- scratch.js
+++ scratch.js
@@ -6,8 +6,17 @@ let txt = ''
 
 txt = `géante est réveillée ` //{réveiller}
 txt = `vous exécutez qu'il active la version` //{précieux}
+txt = `bouillonnant` //{bouillir}
+txt = `Respirez normalement.` //{respirer}
+txt = ` qu'on respire l'air .` //{respirer}
+txt = ` que l'Iraq a dissimulées aux Nations Unies.` //{dissimuler}
+txt = `des coûts « démontre que le gouvernement exploite »` //{démontrer}
+txt = ` 	Attirez l'animal ` //{attirer}
+txt = `  je nageais ` //{nager}
 
 /*
+Malédiction évitée {éviter}
+des chiens qui aboient {aboyer}
 */
 // console.log(nlp('endormir').verbs().conjugate())
 let doc = nlp(txt).debug()
