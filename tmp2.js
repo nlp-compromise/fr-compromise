81aa749cfd21ae88b90045512e520da29a24638c
spencer kelly <spencerkelly86@gmail.com>
2022-09-13 20:31:00 -0400

    accent fixes in present tense

--- tmp2.js
+++ tmp2.js
@@ -2,13 +2,15 @@ import data from '/Users/spencer/mountain/fr-compromise/data/models/verb/present
 
 let out = {}
 Object.keys(data).forEach(k => {
-  if (data[k][0] === data[k][4]) {
+  if (data[k][0] === data[k][3]) {
     out[k] = data[k]
 
   }
-  if (k.match(" ")) {
-    console.log(k)
-  }
+  data[k].forEach(str => {
+    if (!str.startsWith(k.substr(0, 2))) {
+      out[k] = data[k]
+    }
+  })
 })
 
 console.log(JSON.stringify(out, null, 2))
