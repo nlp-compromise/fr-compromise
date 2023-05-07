9f138b3b9885e67220f1fe11cb0cbfc8a2f99ae1
spencer kelly <spencerkelly86@gmail.com>
2023-05-07 15:29:46 -0400

    first builds

--- plugins/dates/rollup.config.js
+++ plugins/dates/rollup.config.js
@@ -1,5 +1,4 @@
 import { terser } from 'rollup-plugin-terser'
-import sizeCheck from 'rollup-plugin-filesize-check'
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 const opts = { keep_classnames: true, module: true }
@@ -8,16 +7,16 @@ export default [
   {
     input: 'src/plugin.js',
     output: [{ file: 'builds/fr-compromise-dates.cjs', format: 'umd', name: 'frCompromiseDates' }],
-    plugins: [nodeResolve(), sizeCheck({ expect: 236, warn: 15 })],
+    plugins: [nodeResolve()],
   },
   {
     input: 'src/plugin.js',
     output: [{ file: 'builds/fr-compromise-dates.min.js', format: 'umd', name: 'frCompromiseDates' }],
-    plugins: [nodeResolve(), terser(opts), sizeCheck({ expect: 113, warn: 15 })],
+    plugins: [nodeResolve(), terser(opts)],
   },
   {
     input: 'src/plugin.js',
     output: [{ file: 'builds/fr-compromise-dates.mjs', format: 'esm' }],
-    plugins: [nodeResolve(), terser(opts), sizeCheck({ expect: 113, warn: 15 })],
+    plugins: [nodeResolve(), terser(opts)],
   }
 ]
