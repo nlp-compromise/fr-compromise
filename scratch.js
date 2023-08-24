92db6aea4136b75e1136bbbe9fb4bc11016c7f56
spencer kelly <spencermountain@gmail.com>
2023-08-24 14:10:33 -0400

    more present-tense verbs

--- scratch.js
+++ scratch.js
@@ -4,12 +4,12 @@ nlp.verbose('tagger')
 
 */
 
-
 // console.log(nlp('essayer').verbs().conjugate())
 
-let root = 'errer'
+let root = 'jaillir'
 let arr = [
-  // mauvais
+  // 'Je montre la direction',
+  `Elle s'immisce dans la conversation`,
   // 'Elle a eu une mauvaise expérience',
   // devenir
   // 'Elle est devenue une célèbre', //passe-compose
@@ -23,7 +23,6 @@ let arr = [
   // accroupir
   // `Elle s'est accroupie derrière l'arbre`, //passe anterior
 
-
   // ménage
   // `Les tâches ménagères `,
 
@@ -35,7 +34,7 @@ let arr = [
   // `une collection de <vieilles> photographies`,
 
   // promouvoir
-  // `Elle a été promue à un poste`, // 
+  // `Elle a été promue à un poste`, //
 
   // pleuvoir
   // `quand il pleut `,
@@ -46,7 +45,6 @@ let arr = [
   // rôtir
   // `Elle a rôti une dinde`, //passe compose
 
-
   // soupirer
   // `Elle soupira `, //passe simple
 
@@ -56,7 +54,6 @@ let arr = [
   // // chanceler
   // `Il <chancela> chez lui `,
 
-
   // épais
   // `une couverture épaisse`,
 
@@ -64,15 +61,13 @@ let arr = [
   // `Elle essaie de parler `,
 
   // errer
-  `Le vieil homme <erra> et se perdit.`, //passe simple
+  `Le vieil homme <erra> et se perdit.` //passe simple
   // ["devenir", "become", "Verb", "She <became> a famous singer after years of practice.", "Elle est devenue une célèbre chanteuse après des années de pratique."],
   // ["accroupir", "crouch", "Verb", "She <crouched> behind the tree to hide.", "Elle s'est accroupie derrière l'arbre pour se cacher."],
 
   // ["endormi", "asleep", "Adjective", "I love listening to music while falling <asleep>.", "J'aime écouter de la musique en m'endormant."],
   // ["mauvais", "bad", "Adjective", "She had a <bad> experience with her previous boss.", "Elle a eu une mauvaise expérience avec son ancien patron."],
   // ["épais", "thick", "Adjective", "The book has a <thick> cover.", "Le livre a une couverture épaisse."],
-
-
 
   // ['Il pêche la truite tous', 'pêcher'],
   // [`L'équipe a été vaincue lors du match final`, 'vaincre'],
@@ -98,15 +93,14 @@ let arr = [
 // let [fr, en, pos, enTxt, frTxt] = arr[0]
 
 // console.log(fr, pos)
-let doc = nlp(arr[0]).debug()
-doc.match(`{${root}}`).debug()
+// let doc = nlp(arr[0]).debug()
+// doc.match(`{${root}}`).debug()
 console.log(nlp(root).verbs().conjugate())
 
 // console.log(doc.verbs().conjugate())
 // doc.verbs().toPastTense().debug()
 // doc.numbers().toNumber()
 // doc.debug()
-
 
 // let doc = nlp('4th sept')
 // let m = doc.match('[<date>#Value] [<month>#Month]')
